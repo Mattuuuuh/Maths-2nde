7a50ff9b2274e9d2a1b741ee5102a6b3797ee3c3
Mattuuuuh <matthieu.haeberle@epfl.ch>
2024-12-07 21:50:00 +0100

    ex3 sol.

--- Statistiques/dm2/generate.py
+++ Statistiques/dm2/generate.py
@@ -36,7 +36,6 @@ def randomize():
 
 
 def generate(seed):
-    FILE_NAME = f"adr/vars_{seed}.adr"
 
     CONTENT = newcommand("", "\seed", seed)
 
@@ -120,9 +119,86 @@ def generate(seed):
     CONTENT = newcommand(CONTENT, "\EFFDISPd", EffDispd)
     CONTENT = newcommand(CONTENT, "\EFFDISPe", EffDispe)
 
+    
+    # EX 1 SOLUTION
 
+    # EX 2 SOLUTION
+
+    # EX 4a SOLUTION
+
+    vala = Conca+.5
+    valb = Conca+1.5
+    valc = Conca+2.5
+    vald = Conca+3.5
+
+    CONTENT = newcommand(CONTENT, "\\vala", comma(vala))
+    CONTENT = newcommand(CONTENT, "\\valb", comma(valb))
+    CONTENT = newcommand(CONTENT, "\\valc", comma(valc))
+    CONTENT = newcommand(CONTENT, "\\vald", comma(vald))
+    
+    N = EffConca + EffConcb + EffConcc + EffConcd
+    X = [vala]*EffConca + [valb]*EffConcb + [valc]*EffConcc + [vald]*EffConcd
+    assert len(X) == N
+    
+    Concaverage = comma(np.round(np.mean(X),2))
+    Concstd = comma(np.round(np.std(X),2))
+    Concmedian = comma(np.round(np.median(X),1))
+    Concfirstquartile = X[int(np.ceil(N/4))]
+    Concthirdquartile = X[int(np.ceil(3*N/4))]
+    Concecart = Concthirdquartile - Concfirstquartile
+    if Concecart == int(Concecart) : Concecart=int(Concecart)
+
+
+    CONTENT = newcommand(CONTENT, "\Concaverage", Concaverage)
+    CONTENT = newcommand(CONTENT, "\Concstd", Concstd)
+    CONTENT = newcommand(CONTENT, "\Concmedian", Concmedian)
+    CONTENT = newcommand(CONTENT, "\Concfirstquartile", comma(Concfirstquartile))
+    CONTENT = newcommand(CONTENT, "\Concthirdquartile", comma(Concthirdquartile))
+    CONTENT = newcommand(CONTENT, "\Concecart", comma(Concecart))
+
+    # EX 4b SOLUTION
+   
+    vala = Dispa+.5
+    valb = Dispa+1.5
+    valc = Dispa+2.5
+    vald = Dispd+.5
+    vale = Dispd+1.5
+
+    CONTENT = newcommand(CONTENT, "\\valA", comma(vala))
+    CONTENT = newcommand(CONTENT, "\\valB", comma(valb))
+    CONTENT = newcommand(CONTENT, "\\valC", comma(valc))
+    CONTENT = newcommand(CONTENT, "\\valD", comma(vald))
+    CONTENT = newcommand(CONTENT, "\\valE", comma(vale))
+    
+    N = EffDispa + EffDispb + EffDispc + EffDispd + EffDispe
+    X = [vala]*EffDispa + [valb]*EffDispb + [valc]*EffDispc + [vald]*EffDispd + [vale]*EffDispe
+    assert len(X) == N
+    
+    Dispaverage = comma(np.round(np.mean(X),2))
+    Dispstd = comma(np.round(np.std(X),2))
+    Dispmedian = comma(np.round(np.median(X),1))
+    Dispfirstquartile = X[int(np.ceil(N/4))]
+    Dispthirdquartile = X[int(np.ceil(3*N/4))]
+    Dispecart = Dispthirdquartile - Dispfirstquartile
+    if Dispecart == int(Dispecart) : Dispecart=int(Dispecart)
+
+    CONTENT = newcommand(CONTENT, "\Dispaverage", Dispaverage)
+    CONTENT = newcommand(CONTENT, "\Dispstd", Dispstd)
+    CONTENT = newcommand(CONTENT, "\Dispmedian", Dispmedian)
+    CONTENT = newcommand(CONTENT, "\Dispfirstquartile", comma(Dispfirstquartile))
+    CONTENT = newcommand(CONTENT, "\Dispthirdquartile", comma(Dispthirdquartile))
+    CONTENT = newcommand(CONTENT, "\Dispecart", comma(Dispecart))
+
+    # WRITE TO FILE
+    
+    FILE_NAME = f"adr/vars_{seed}.adr"
+
+    
     with open(FILE_NAME, 'w') as file:
         file.write(CONTENT)
+
+
+    # COMPILE LATEX TWICE
 
     INPUTS = "\input{preamble.tex} \input{"+FILE_NAME+"} \input{dm2.tex}"
     PARAMETER1 = f"-output-directory=out"
